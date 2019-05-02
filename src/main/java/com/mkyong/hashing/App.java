9e04632b183c3a8a366d0945eafcc56ea7db6e9c
KRutuja <rutuja.ronghe@gmail.com>
2019-05-02 16:42:30 +0530

    Second Commit

--- src/main/java/com/mkyong/hashing/App.java
+++ src/main/java/com/mkyong/hashing/App.java
@@ -10,6 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
 System.out.println( "How you doing!" );
+System.out.println( "How are you!" );
     }
 public boolean isPrime(int n) {
 
